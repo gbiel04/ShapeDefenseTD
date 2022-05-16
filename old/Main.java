3763c76f5a03293b29203e013322128373ef342a
gbiel04 <gbezerra@etech-nyc.org>
2022-05-16 11:13:38 -0400

    swapped out template

---
+++ old/Main.java
@@ -0,0 +1,10 @@
+//GROUP V - GABRIEL & VICTORIA - SHAPE DEFENSE TD
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Starting Main.main()... \t");
+		Game game = new Game();
+    game.play();   
+  }
+	
+}
--- replit.nix
+++ replit.nix
@@ -1,8 +1,6 @@
 { pkgs }: {
-    deps = [
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
+  deps = [
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
 }
